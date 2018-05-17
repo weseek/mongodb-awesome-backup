f50417783172a6494ed144fc516430b63d93d048
Ryu Sato <ryu@weseek.co.jp>
2018-05-18 02:59:14 +0900

    add cronmode

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -e
 
 # start script
-CWD=`/usr/bin/dirname $0`
-cd $CWD
-
-for arg in $@; do
-  ./${arg}.sh
-done
+CRONMODE=${CRONMODE:-false}
+if $CRONMODE ; then
+  echo "=== started in cron mode `/bin/date "+%Y/%m/%d %H:%M:%S"` ==="
+  crontab -l
+  exec crond -f -d 8
+else
+  CWD=`/usr/bin/dirname $0`
+  cd $CWD
+  
+  for arg in $@; do
+    ./${arg}.sh
+  done
+fi
