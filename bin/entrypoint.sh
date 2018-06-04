a5ca372a9a2d2afbbed6cf46de6e744fe5762ab7
Ryu Sato <ryu@weseek.co.jp>
2018-06-05 02:07:17 +0900

    enable to specify cron expression by environment

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,6 +3,11 @@
 # start script
 CRONMODE=${CRONMODE:-false}
 if $CRONMODE ; then
+  if [ -z "${CRON_EXPRESSION}" ]; then
+    echo 'ERROR: The environment variable CRON_EXPRESSION must be specified in cron mode.'
+    exit 255;
+  fi
+  echo "${CRON_EXPRESSION} /opt/bin/command_exec.sh $@" > /var/spool/cron/crontabs/root
   echo "=== started in cron mode `/bin/date "+%Y/%m/%d %H:%M:%S"` ==="
   crontab -l
   exec crond -f -d 8
