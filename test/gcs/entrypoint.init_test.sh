745cadece97d4b9a8d232802160b2d2f64109922
Ryu Sato <ryu@weseek.co.jp>
2019-03-19 00:13:45 +0900

    FB内容を既存のS3テストへも適用

--- test/gcs/entrypoint.init_test.sh
+++ test/gcs/entrypoint.init_test.sh
@@ -3,7 +3,9 @@
 # Expand all variables in "/tmp/.boto"
 envsubst < /tmp/.boto > /root/.boto
 
-# REMOVE ALL OBJECTS in GCS bucket and copy fixture file which is used in test restoring
+# REMOVE ALL OBJECTS in GCS bucket
 gsutil ls ${TARGET_BUCKET_URL}
 gsutil rm -rf ${TARGET_BUCKET_URL}
+
+# Copy fixture file which is used in test restoring
 gsutil cp dummy-backup-20180622.tar.bz2 ${TARGET_BUCKET_URL}
