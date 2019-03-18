a88398eccf8a4c7ed712c6822ae255d360fe5d6f
Ryu Sato <ryu@weseek.co.jp>
2019-03-18 23:30:16 +0900

    FB対応

---
+++ test/gcs/entrypoint.init_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# Expand all variables in "/tmp/.boto"
+envsubst < /tmp/.boto > /root/.boto
+
+# REMOVE ALL OBJECTS in GCS bucket and copy fixture file which is used in test restoring
+gsutil ls ${TARGET_BUCKET_URL}
+gsutil rm -rf ${TARGET_BUCKET_URL}
+gsutil cp dummy-backup-20180622.tar.bz2 ${TARGET_BUCKET_URL}
