9398c1e5482da2726d6a196560631ee535527a13
Syunsuke Komma <syunsuke@weseek.co.jp>
2019-06-27 10:55:59 +0900

    fix command to clean up the bucket

--- test/gcs/entrypoint.init_test.sh
+++ test/gcs/entrypoint.init_test.sh
@@ -5,7 +5,7 @@ envsubst < /tmp/.boto > /root/.boto
 
 # REMOVE ALL OBJECTS in GCS bucket
 gsutil ls ${TARGET_BUCKET_URL}
-gsutil rm -rf ${TARGET_BUCKET_URL}
+gsutil rm -rf ${TARGET_BUCKET_URL}**
 
 # Copy fixture file which is used in test restoring
 gsutil cp dummy-backup-20180622.tar.bz2 ${TARGET_BUCKET_URL}
