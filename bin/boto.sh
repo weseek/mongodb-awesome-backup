6ec0f6a30fe87946cc72553171a17316ed9dd7ca
Ryu Sato <ryu@weseek.co.jp>
2020-02-27 00:34:06 +0900

    disable credential passing when auth by .boto file

--- bin/boto.sh
+++ bin/boto.sh
@@ -8,10 +8,14 @@ GCLOUDCLI="/root/google-cloud-sdk/bin/gcloud"
 MOUNT="/mab"
 
 if [ -n "${GCP_PROJECT_ID}" ] && [ -n "${GCP_SERVICE_ACCOUNT_KEY_JSON_PATH}" ]; then
+  echo '[DEBUG] Using GCP service account authorization'
+
   # Using GCP service account authorization
   ${GCLOUDCLI} auth activate-service-account --key-file="${GCP_SERVICE_ACCOUNT_KEY_JSON_PATH}"
   ${GCLOUDCLI} --quiet config set project ${GCP_PROJECT_ID}
 elif [ -n "${GCP_ACCESS_KEY_ID}" ] && [ -n "${GCP_SECRET_ACCESS_KEY}" ]; then
+  echo '[DEBUG] Using HMAC authorization'
+
   # Using HMAC authorization
   cat <<- HERE > /root/.boto
 [Credentials]
@@ -36,9 +40,19 @@ default_project_id = $GCP_PROJECT_ID
 [OAuth2]
 HERE
 elif [ -f ${MOUNT}/.boto ]; then
+  echo '[DEBUG] Using mounted `.boto` file authorization'
+  
+  # Disable credential passing (ref. https://cloud.google.com/storage/docs/gsutil_install)
+  ${GCLOUDCLI} config set pass_credentials_to_gsutil false
+
   # Using mounted `.boto` file authorization
   cp ${MOUNT}/.boto /root/.boto
 elif [ ! -f ${MOUNT}/.boto ]; then
+  echo '[DEBUG] Using interactive authorization'
+  
+  # Disable credential passing (ref. https://cloud.google.com/storage/docs/gsutil_install)
+  ${GCLOUDCLI} config set pass_credentials_to_gsutil false
+
   # Using interactive authorization
   if [ ! -d ${MOUNT} ]; then mkdir -p ${MOUNT}; fi
   ${GCSCLI} config -o ${MOUNT}/.boto
