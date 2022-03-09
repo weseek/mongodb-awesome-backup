c0b5d56b1a971b45300436e3cb77d8d1ffe3b9b1
Ryu Sato <ryu@weseek.co.jp>
2022-03-10 01:42:26 +0900

    specify file which will be pruned by wildcard, and skip checking existence of backup file before pruning

--- bin/prune.sh
+++ bin/prune.sh
@@ -15,7 +15,7 @@ PAST=`create_past_yyyymmdd ${DELETE_TARGET_DAYS_LEFT}`
 
 # check the existence of past file
 # if it exists, delete it
-TARBALL_PAST="${BACKUPFILE_PREFIX}-${PAST}.tar.bz2"
+TARBALL_PAST="${BACKUPFILE_PREFIX}-${PAST}*.tar.bz2"
 
 if [ `echo $TARGET_BUCKET_URL | cut -f1 -d":"` == "s3" ]; then
   s3_delete_file_if_delete_backup_day ${TARGET_BUCKET_URL}${TARBALL_PAST} ${DELETE_TARGET_DAYS_LEFT} ${DELETE_DEVIDE}
